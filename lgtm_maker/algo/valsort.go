8b091dfd97ebf8fbaa73f6d712e788754a30d0ef
ciela <zektbach@gmail.com>
2014-12-06 18:54:07 +0900

    Add algo package

---
+++ lgtm_maker/algo/valsort.go
@@ -0,0 +1,34 @@
+package algo
+
+import (
+	"image/color"
+	"sort"
+)
+
+type ValSorter struct {
+	Keys []color.Color
+	Vals []int
+}
+
+func NewValSorter(m map[color.Color]int) *ValSorter {
+	vs := &ValSorter{
+		Keys: make([]color.Color, 0, len(m)),
+		Vals: make([]int, 0, len(m)),
+	}
+	for k, v := range m {
+		vs.Keys = append(vs.Keys, k)
+		vs.Vals = append(vs.Vals, v)
+	}
+	return vs
+}
+
+func (vs *ValSorter) Sort() {
+	sort.Sort(vs)
+}
+
+func (vs *ValSorter) Len() int           { return len(vs.Vals) }
+func (vs *ValSorter) Less(i, j int) bool { return vs.Vals[i] < vs.Vals[j] }
+func (vs *ValSorter) Swap(i, j int) {
+	vs.Vals[i], vs.Vals[j] = vs.Vals[j], vs.Vals[i]
+	vs.Keys[i], vs.Keys[j] = vs.Keys[j], vs.Keys[i]
+}
